ffdebef20fa84c4fc9c91b49d7e68bbb151e8e4f
RonTheWebDev <ron@creatiquemedia.com>
2023-01-21 11:29:43 -0600

    File Changes

---
+++ .next/static/KsLzxWxCksmZzUrJ5kG79/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-1dc32490a9b0c987.js","static/chunks/pages/index-6c9ba084e87d5ddd.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
